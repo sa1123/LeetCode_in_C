66876b5776c22315a0a72f1c0ae263ec771dd20c
Sahil <51375919+sa1123@users.noreply.github.com>
2023-03-16 00:14:59 +0530

    https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/ better

--- LL-binaryToDecimal.c
+++ LL-binaryToDecimal.c
@@ -11,11 +11,7 @@ int getDecimalValue(struct ListNode* head){
     int ans = 0;
 
     while(head != NULL){
-        if(head->val == 0){
-            ans*=2;
-        }else{
-            ans = ans * 2 + 1;
-        }
+        ans = ans * 2 + head->val;
         head = head->next;
     }
     return ans;
